9af2222533393ececc2736b32a748331a53dd3d0
zanglei <0620isabella@163.com>
2019-12-05 16:11:24 +0800

    策略模式

--- strategy-pattern/src/main/java/nature/wildcode/koala/strategy/FlyWithWings.java
+++ strategy-pattern/src/main/java/nature/wildcode/koala/strategy/FlyWithWings.java
@@ -2,6 +2,6 @@ package nature.wildcode.koala.strategy;
 
 public class FlyWithWings implements Fly {
     public void fly() {
-        System.out.println("带翅膀");
+        System.out.println("边飞边睡");
     }
 }
--- strategy-pattern/src/main/java/nature/wildcode/koala/strategy/UglyLittleDuck.java
+++ strategy-pattern/src/main/java/nature/wildcode/koala/strategy/UglyLittleDuck.java
@@ -2,6 +2,6 @@ package nature.wildcode.koala.strategy;
 
 public class UglyLittleDuck extends AbstractDuck {
     public void display() {
-        System.out.println("我是丑小鸭");
+        System.out.println("其实我是天鹅，你们这帮SB");
     }
 }
